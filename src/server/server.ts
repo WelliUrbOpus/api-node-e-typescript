692594e9def9a81e7d6009f3cfc373b62481a365
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-02-26 16:10:37 -0300

    feat: added code style configs
    Style configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 
 const server = express();
 
+
 server.get('/', (req, res) => {
-    return res.send('Ola, DEV')
+  return res.send('Ola, DEV');
 });
 
 export { server };
