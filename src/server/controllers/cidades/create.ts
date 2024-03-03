aba5387ce58f037628f7bc58bd679401ca5e0f35
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-03 19:29:03 -0300

    Aula 10
    Aula 10

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -2,32 +2,35 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
 
-
 interface ICidade {
   name: string;
+  estado: string;
 }
 
 const bodyValidation = yup.object().shape({
-  name: yup.string().required().min(3)
+  name: yup.string().required().min(3),
+  estado: yup.string().required().min(3)
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-  let validateData: ICidade | undefined  = undefined;
+  let validateData: ICidade | undefined = undefined;
   //const data: ICidade = req.body;
 
   try {
 
-    validateData = await bodyValidation.validate(req.body);
+    validateData = await bodyValidation.validate(req.body, { abortEarly: false });
 
   } catch (error) {
 
-    const yuoError = error as yup.ValidationError;
+    const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {};
 
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: {
-        default: yuoError.message,
-      }
+    yupError.inner.forEach(error => {
+      if (error.path === undefined) return;
+      validationErrors[error.path] = error.message;
     });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors: validationErrors });
 
   }
 
