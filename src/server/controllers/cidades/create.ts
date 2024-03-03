8b58f2b9b232f3b184c73141f5fa720ebc0df4e8
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-03 09:36:51 -0300

    Aula 9
    Aula 9

--- src/server/controllers/cidades/create.ts
+++ src/server/controllers/cidades/create.ts
@@ -1,13 +1,37 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+
 
 interface ICidade {
-  nome: string;
+  name: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation = yup.object().shape({
+  name: yup.string().required().min(3)
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined  = undefined;
   //const data: ICidade = req.body;
 
-  console.log(req.body);
+  try {
+
+    validateData = await bodyValidation.validate(req.body);
+
+  } catch (error) {
+
+    const yuoError = error as yup.ValidationError;
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yuoError.message,
+      }
+    });
+
+  }
+
+  console.log(validateData);
 
   return res.send('Create!');
 };
