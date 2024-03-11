ed4e6610f57985e670e6dbad522a9ead05b33822
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-10 22:43:54 -0300

    Atualização de dependencies
    Atualização de dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
 
-server.listen(process.env.PORT || 3333, () => {
-    console.log(`\nApp rodando, \nna porta: ${process.env.PORT || 3333}`);
-    console.log(`Servidor está rodando em http://localhost:${process.env.PORT || 3333}`);
-});
+
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => {
+        console.log(`\nApp rodando, \nna porta: ${process.env.PORT || 3333}`);
+        console.log(`Servidor está rodando em http://localhost:${process.env.PORT || 3333}`);
+    });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+
+    Knex.migrate.latest().then(() => {
+        startServer();
+    }).catch(console.log);
+} else {
+    startServer();
+}
