ee60eca9fb8bc5f28f632abd270c1f3217094243
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-13 08:09:11 -0300

    Correção dos requisitos de testes
    Adicionado mais condições de testes

--- tests/pessoas/create.test.ts
+++ tests/pessoas/create.test.ts
@@ -65,21 +65,93 @@ describe('Pessoas - Create', () => {
                 cidadeId: cidadeId
             });
 
-        expect(res1.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
-        expect(res1.body).toHaveProperty('errors.default');
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body');
     });
 
     it('Criar registro com lastName curto', async () => {
         const res1 = await testServer
             .post('/pessoas')
             .send({
-                firstName: 'Wellington',
+                firstName: 'LastNameCurto',
                 lastName: 'da',
                 email: 'welligtonlastnamecurto@gmail.com',
                 cidadeId: cidadeId
             });
 
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body');
+    });
+
+    it('Criar registro sem e-mail', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                firstName: 'Wellington',
+                lastName: 'sem email',
+                email: '',
+                cidadeId: cidadeId
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body');
+    });
+
+    it('Criar registro com e-mail inválido', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                firstName: 'Wellington',
+                lastName: 'da',
+                email: 'welligtongmail.com',
+                cidadeId: cidadeId
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body');
+    });
+
+    it('Criar registro sem cidadeId', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                firstName: 'Wellington',
+                lastName: 'sem cidadeId',
+                email: 'welligtonsemcidadeid@gmail.com'
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body');
+    });
+
+    it('Criar registro com cidadeId inválida', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                firstName: 'Wellington',
+                lastName: 'sem cidadeId',
+                email: 'welligtonsemcidadeid@gmail.com',
+                cidadeId: 9999999
+            });
+
         expect(res1.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
         expect(res1.body).toHaveProperty('errors.default');
     });
+
+    it('Criar registro sem enviar nenhuma propiedade', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                firstName: '',
+                lastName: '',
+                email: '',
+                cidadeId: ''
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body');
+    });
+
+
+
 });
