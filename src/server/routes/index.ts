09308bf924f819517378ba1d79e7e9081d7a2264
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-16 20:53:48 -0300

    Autenticação de rotas
    API Rest, Node e Typescript: #37 - Adicionar autenticação em rotas com express

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,7 @@
 //import path from 'path';
 import { Router } from 'express';
 import { CidadesController, NivelUsuariosController, PessoasController, UsuariosController } from './../controllers';
+import { ensureAuthenticated } from '../shared/middleware';
 
 
 
@@ -12,25 +13,25 @@ router.get('/', (_, res) => {
 });
 
 
-router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
-router.post('/cidades', CidadesController.createValidation, CidadesController.create);
-router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
-router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
-router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
+router.get('/cidades',ensureAuthenticated, CidadesController.getAllValidation, CidadesController.getAll);
+router.post('/cidades',ensureAuthenticated, CidadesController.createValidation, CidadesController.create);
+router.get('/cidades/:id',ensureAuthenticated, CidadesController.getByIdValidation, CidadesController.getById);
+router.put('/cidades/:id',ensureAuthenticated, CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id',ensureAuthenticated, CidadesController.deleteByIdValidation, CidadesController.deleteById);
 
-router.get('/pessoas', PessoasController.getAllValidation, PessoasController.getAll);
-router.post('/pessoas', PessoasController.createValidation, PessoasController.create);
-router.get('/pessoas/:id', PessoasController.getByIdValidation, PessoasController.getById);
-router.put('/pessoas/:id', PessoasController.updateByIdValidation, PessoasController.updateById);
-router.delete('/pessoas/:id', PessoasController.deleteByIdValidation, PessoasController.deleteById);
+router.get('/pessoas',ensureAuthenticated, PessoasController.getAllValidation, PessoasController.getAll);
+router.post('/pessoas',ensureAuthenticated, PessoasController.createValidation, PessoasController.create);
+router.get('/pessoas/:id',ensureAuthenticated, PessoasController.getByIdValidation, PessoasController.getById);
+router.put('/pessoas/:id',ensureAuthenticated, PessoasController.updateByIdValidation, PessoasController.updateById);
+router.delete('/pessoas/:id',ensureAuthenticated, PessoasController.deleteByIdValidation, PessoasController.deleteById);
 
 //router.post('/leveluser', NivelUsuariosController.createValidation, NivelUsuariosController.create);
-router.get('/leveluser', NivelUsuariosController.getAllValidation, NivelUsuariosController.getAll);
-router.get('/leveluser/:id', NivelUsuariosController.getByIdValidation, NivelUsuariosController.getById);
+router.get('/leveluser',ensureAuthenticated, NivelUsuariosController.getAllValidation, NivelUsuariosController.getAll);
+router.get('/leveluser/:id',ensureAuthenticated, NivelUsuariosController.getByIdValidation, NivelUsuariosController.getById);
 
-router.get('/usuarios', UsuariosController.getAllValidation, UsuariosController.getAll);
+router.get('/usuarios',ensureAuthenticated, UsuariosController.getAllValidation, UsuariosController.getAll);
+router.put('/usuarios/update',ensureAuthenticated, UsuariosController.updateByIdValidation, UsuariosController.updateById);
 router.post('/cadastrar', UsuariosController.createValidation, UsuariosController.create);
 router.get('/entrar', UsuariosController.signInValidation, UsuariosController.signIn);
-router.put('/usuarios/update', UsuariosController.updateByIdValidation, UsuariosController.updateById);
 
 export { router };
