638d8bf6ae4779f969c2709af22f8b2f7c3bbc5d
WelliUrbOpus <wellington.urbano@opuspac.com.br>
2024-03-18 15:43:13 -0300

    Subir para produção
    Instalação de pg e configurações necessárias para produção

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -24,6 +24,21 @@ export const test: Knex.Config = {
     ...development,
     connection: ':memory:',
 };
+
 export const production: Knex.Config = {
-    ...development,
+    client: 'pg',
+    migrations: {
+        directory: path.resolve(__dirname, '..', 'migrations')
+    },
+    seeds: {
+        directory: path.resolve(__dirname, '..', 'seeds')
+    },
+    connection: {
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT || 5435),
+        ssl: { rejectUnauthorized: false },
+    },
 };
